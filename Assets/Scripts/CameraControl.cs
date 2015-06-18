a1492d9ca183f3e0750b4c6620e05ca2b95f8c5d
Colin Moore <colin.moore42@googlemail.com>
2015-06-18 14:35:48 +0100

    The entire build interface is now gesture controlled -  all buttons have been removed from the UI.
    Pan forwards: single finger drag-up & hold.
    Pan backwards: single finger drag-down & hold.
    Rotate Right 90: swipe right.
    Rotate Left 90: swipe left.
    
    Removed single finger tap to pan forwards.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System;
 using System.Collections;
+using System.Timers;
 
 public class CameraControl : MonoBehaviour {
 
@@ -12,13 +13,20 @@ public class CameraControl : MonoBehaviour {
 
 
 	Vector3 mouseButtonDownScreenPos;
+	Vector3 pinchDelta = new Vector3( 0, 0, 0);
 
 	Touch touchZero;
 	Touch touchOne;
+	Touch touchZeroPan;
 
 	bool bTouchSizing = false;
+	bool bTouchPanning = false;
+	bool bTouchPanningDirForward = false;
+	bool bTouchRotate = false;
 
 	GameObject roomScalerGFX;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -61,38 +69,89 @@ public class CameraControl : MonoBehaviour {
 		roomScalerGFX.GetComponent<MeshFilter>().mesh.RecalculateBounds();
 		
 	
-	
+
+		Material mat = Resources.Load ("Materials/RoomScalarCursor", typeof(Material)) as Material; 
+
+		Material [] mats = new Material[1];
+		mats[0] = mat;
+		roomScalerGFX.GetComponent<Renderer> ().materials = mats;
+
+		roomScalerGFX.gameObject.SetActive (false);
+
+
+		InvokeRepeating("OnTimedEvent_PanCamera", 0.0f, 0.4f);
+	}
+
+	private void OnTimedEvent_PanCamera()
+	{
+		if (bTouchPanning == true ) 
+		{
+			MapGen mapgen = GameObject.Find ("Map").GetComponent<MapGen> ();
+			if (mapgen != null) {
+
+				if ( bTouchPanningDirForward)
+				{
+					Vector2 targetPos = mapgen.tileCursorTarget + new Vector2 ((cameraOffset.x == 0f) ? 0f : -Mathf.Sign (cameraOffset.x), (cameraOffset.z == 0f) ? 0f : -Mathf.Sign (cameraOffset.z));
+					if (mapgen.validDigLocation (targetPos)) {
+						mapgen.tileCursorTarget = targetPos;
+					}
+				}
+				else{
+					Vector2 targetPos = mapgen.tileCursorTarget -  new Vector2 ((cameraOffset.x == 0f) ? 0f : -Mathf.Sign (cameraOffset.x), (cameraOffset.z == 0f) ? 0f : -Mathf.Sign (cameraOffset.z));
+					if (mapgen.validDigLocation (targetPos)) {
+						mapgen.tileCursorTarget = targetPos;
+					}
+				}
+			}
+		}
 	}
 
 	void OnGUI() {
 
 		GUI.skin.label.fontSize = 64;
 
-
 		MapGen mapgen = GameObject.Find("Map").GetComponent<MapGen>();
 		if (mapgen != null) 
 		{
-			if (GUI.Button (new Rect (Screen.width * .2f, Screen.height * 0.85f, Screen.width * .19f, Screen.height * 0.09f), "<")) {
-				targetCameraOffset = Quaternion.Euler(0, -90, 0) * targetCameraOffset;
-				targetRotationIncrement = -10;
-			}
-			if (GUI.Button (new Rect (Screen.width * .4f, Screen.height * 0.85f, Screen.width * .19f, Screen.height * 0.09f), "X")) {
+			/*
+			if (GUI.Button (new Rect (Screen.width * .4f, Screen.height * 0.85f, Screen.width * .2f, Screen.height * 0.12f), "DIG")) {
 				mapgen.EditTile (mapgen.tileCursor.x, mapgen.tileCursor.y);
 			}
-			if (GUI.Button (new Rect (Screen.width * .6f, Screen.height * 0.85f, Screen.width * .19f, Screen.height * 0.09f), ">")) {
-				targetCameraOffset = Quaternion.Euler(0, +90, 0) * targetCameraOffset;
-				targetRotationIncrement = 10;
-			}
+			*/
 
 
-			if (GUI.Button (new Rect (Screen.width * .4f, Screen.height * 0.75f, Screen.width * .19f, Screen.height * 0.09f), "^")) 
+		
+
+			if (Application.platform != RuntimePlatform.Android) 
 			{
-				Vector2 targetPos = mapgen.tileCursorTarget + new Vector2((cameraOffset.x == 0f) ? 0f : -Mathf.Sign(cameraOffset.x), (cameraOffset.z == 0f) ? 0f :-Mathf.Sign(cameraOffset.z));
-				if ( mapgen.validDigLocation(targetPos))
+				if (cameraOffset == targetCameraOffset) {
+					if (GUI.Button (new Rect (Screen.width * .05f, Screen.height * 0.01f, Screen.width * .2f, Screen.height * 0.11f), "<")) {
+						targetCameraOffset = Quaternion.Euler(0, -90, 0) * targetCameraOffset;
+						targetRotationIncrement = -10;
+					}
+				}
+
+				if (GUI.Button (new Rect (Screen.width * .4f, Screen.height * 0.01f, Screen.width * .2f, Screen.height * 0.11f), "^")) 
 				{
-					mapgen.tileCursorTarget = targetPos;
+					Vector2 targetPos = mapgen.tileCursorTarget + new Vector2((cameraOffset.x == 0f) ? 0f : -Mathf.Sign(cameraOffset.x), (cameraOffset.z == 0f) ? 0f :-Mathf.Sign(cameraOffset.z));
+					if ( mapgen.validDigLocation(targetPos))
+					{
+						mapgen.tileCursorTarget = targetPos;
+					}
+				}
+
+				if (cameraOffset == targetCameraOffset) {
+					if (GUI.Button (new Rect (Screen.width * .75f, Screen.height * 0.01f, Screen.width * .2f, Screen.height * 0.11f), ">")) {
+						targetCameraOffset = Quaternion.Euler(0, +90, 0) * targetCameraOffset;
+						targetRotationIncrement = 10;
+					}
 				}
 			}
+
+	
+
+
+
 
 		}
 
@@ -120,9 +179,55 @@ public class CameraControl : MonoBehaviour {
 
 		roomScalerGFX.transform.position = new Vector3 (mapgen.tileCursorTarget.x + 0.5f, 1.1f, mapgen.tileCursorTarget.y + 0.5f);
 
+		if (Application.platform == RuntimePlatform.Android) 
+		{
+			if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				touchZeroPan = Input.GetTouch(0);
+				bTouchRotate = true;
+			}
 
-		if (Application.platform == RuntimePlatform.Android) {
 
+			if (Input.touchCount == 1)
+			{
+				// pan
+				if ( Mathf.Abs ( Input.GetTouch(0).position.y - touchZeroPan.position.y ) > Mathf.Abs ( Input.GetTouch(0).position.x - touchZeroPan.position.x )) // forward/backward quadrant
+				{
+					if ( Mathf.Abs ( Input.GetTouch(0).position.y - touchZeroPan.position.y) > Screen.height * .1f) // min pan threshhold
+					{
+						bTouchPanning = true;
+						bTouchPanningDirForward = (Input.GetTouch(0).position.y  > touchZeroPan.position.y) ? true : false;
+					}
+				}
+
+				// rotate
+				if ( bTouchRotate == true)
+				{
+					if ( Mathf.Abs ( Input.GetTouch(0).position.y - touchZeroPan.position.y ) < Mathf.Abs ( Input.GetTouch(0).position.x - touchZeroPan.position.x )) // forward/backward quadrant
+					{
+						if ( Mathf.Abs ( Input.GetTouch(0).position.x - touchZeroPan.position.x) > Screen.width * .15f) // min rotate threshhold
+						{
+							bTouchRotate = false;
+
+							if ( Input.GetTouch(0).position.x < touchZeroPan.position.x)
+							{
+								targetCameraOffset = Quaternion.Euler(0, -90, 0) * targetCameraOffset;
+								targetRotationIncrement = -10;
+							}
+							else{
+								targetCameraOffset = Quaternion.Euler(0, +90, 0) * targetCameraOffset;
+								targetRotationIncrement = +10;
+							}
+						}
+					}
+				}
+			}
+
+			if ( Input.touchCount != 1)
+			{
+				bTouchRotate = false;
+				bTouchPanning=false;
+			}
 
 
 			if (Input.touchCount == 2 && Input.GetTouch(1).phase == TouchPhase.Began)
@@ -137,15 +242,14 @@ public class CameraControl : MonoBehaviour {
 			if (Input.touchCount == 2)
 			{
 				bTouchSizing = true;
-				Vector3 mouseButtonDownScreenPosVector = new Vector3( 0, 0, 0);
-				mouseButtonDownScreenPosVector.x = Mathf.Abs ( Input.GetTouch(0).position.x - Input.GetTouch(1).position.x ) - Mathf.Abs( touchZero.position.x - touchOne.position.x);
-				mouseButtonDownScreenPosVector.y = Mathf.Abs ( Input.GetTouch(0).position.y - Input.GetTouch(1).position.y ) - Mathf.Abs( touchZero.position.y - touchOne.position.y);
+				pinchDelta.x = Mathf.Abs ( Input.GetTouch(0).position.x - Input.GetTouch(1).position.x ) - Mathf.Abs( touchZero.position.x - touchOne.position.x);
+				pinchDelta.y = Mathf.Abs ( Input.GetTouch(0).position.y - Input.GetTouch(1).position.y ) - Mathf.Abs( touchZero.position.y - touchOne.position.y);
 
-				if ( mouseButtonDownScreenPosVector.x < 0) mouseButtonDownScreenPosVector.x = 0;
-				if ( mouseButtonDownScreenPosVector.y < 0) mouseButtonDownScreenPosVector.y = 0;
+				if ( pinchDelta.x < 0) pinchDelta.x = 0;
+				if ( pinchDelta.y < 0) pinchDelta.y = 0;
 
-				Vector3 widthScaler = new Vector3 (Mathf.RoundToInt ((mouseButtonDownScreenPosVector.x / Screen.width) * 6), 0, 0);
-				Vector3 heightScaler = new Vector3 (0, 0, Mathf.RoundToInt ((mouseButtonDownScreenPosVector.y / Screen.height) * 10));
+				Vector3 widthScaler = new Vector3 (Mathf.RoundToInt ((pinchDelta.x / Screen.width) * 6), 0, 0);
+				Vector3 heightScaler = new Vector3 (0, 0, Mathf.RoundToInt ((pinchDelta.y / Screen.height) * 10));
 				
 				Vector3[] vertices = new Vector3[]
 				{
@@ -171,20 +275,17 @@ public class CameraControl : MonoBehaviour {
 			}
 
 
-			if ( bTouchSizing && Input.touchCount != 2)
+			if ( (bTouchSizing == true) && (Input.touchCount == 0))
 			{
-				roomScalerGFX.gameObject.SetActive (false);
+				//mapgen.EditTile (mapgen.tileCursorTarget.x, mapgen.tileCursorTarget.y);
+				//mapgen.EditTile (mapgen.tileCursor.x, mapgen.tileCursor.y);
+
+		
 				bTouchSizing = false;
 
-				Vector3 mouseButtonDownScreenPosVector = new Vector3( 0, 0, 0);
-				mouseButtonDownScreenPosVector.x = Mathf.Abs ( Input.GetTouch(0).position.x - Input.GetTouch(1).position.x ) - Mathf.Abs( touchZero.position.x - touchOne.position.x);
-				mouseButtonDownScreenPosVector.y = Mathf.Abs ( Input.GetTouch(0).position.y - Input.GetTouch(1).position.y ) - Mathf.Abs( touchZero.position.y - touchOne.position.y);
-				
-				if ( mouseButtonDownScreenPosVector.x < 0) mouseButtonDownScreenPosVector.x = 0;
-				if ( mouseButtonDownScreenPosVector.y < 0) mouseButtonDownScreenPosVector.y = 0;
-
-				Vector3 widthScaler = new Vector3 (Mathf.RoundToInt ((mouseButtonDownScreenPosVector.x / Screen.width) * 6), 0, 0);
-				Vector3 heightScaler = new Vector3 (0, 0, Mathf.RoundToInt ((mouseButtonDownScreenPosVector.y / Screen.height) * 10));
+	
+				Vector3 widthScaler = new Vector3 (Mathf.RoundToInt ((pinchDelta.x / Screen.width) * 6), 0, 0);
+				Vector3 heightScaler = new Vector3 (0, 0, Mathf.RoundToInt ((pinchDelta.y / Screen.height) * 10));
 				
 				Vector3[] vertices = new Vector3[]
 				{
@@ -217,8 +318,10 @@ public class CameraControl : MonoBehaviour {
 					
 				}
 
+	
 				roomScalerGFX.gameObject.SetActive (false);
 			}
+
 
 		} else {
 
@@ -270,12 +373,12 @@ public class CameraControl : MonoBehaviour {
 				Vector3 heightScaler = new Vector3 (0, 0, Mathf.RoundToInt ((mouseButtonDownScreenPosVector.y / Screen.height) * 10));
 			
 				Vector3[] vertices = new Vector3[]
-			{
+				{
 				new Vector3 (-0.5f, 0, -0.5f),
 				new Vector3 (-0.5f, 0, +0.5f),
 				new Vector3 (+0.5f, 0, -0.5f),
 				new Vector3 (+0.5f, 0, +0.5f)
-			};
+				};
 			
 				vertices [0] -= widthScaler;
 				vertices [1] -= widthScaler;
@@ -320,8 +423,7 @@ public class CameraControl : MonoBehaviour {
 		}
 
 		if (cameraOffset != targetCameraOffset) {
-			//float sign = Mathf.Sign(Vector3.Dot(cameraOffset, targetCameraOffset));
-			//Debug.Log (sign);
+
 			cameraOffset = Quaternion.Euler (0, targetRotationIncrement, 0) * cameraOffset;
 		} 
 		else 
